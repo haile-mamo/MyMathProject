e05c7f5c4e956caf798853e749b0b6b0db1561a6
hailemichael mamo <hailemamo93@gmail.com>
2026-02-03 13:20:31 +0300

    Fix: Removed wrong views import in main urls.py

--- math_app/urls.py
+++ math_app/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path 
+from django.urls import path
+from . import views # እዚህ ጋር መኖሩ ትክክል ነው
+
+urlpatterns = [
+    path('', views.home, name='home'),
+]
--- my_math_site/urls.py
+++ my_math_site/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('admin/', admin.site.urls),
+    path('', include('math_app.urls')), 
 ]
+# ከላይ 'from . import views' የሚል መስመር ካለ አጥፋው!
