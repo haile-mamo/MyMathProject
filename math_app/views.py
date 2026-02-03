c13c30c4ac76cee52403b14ec541c005843004f3
hailemichael mamo <hailemamo93@gmail.com>
2026-02-03 13:33:56 +0300

    Fixed action URL and added multiplication logic

--- math_app/views.py
+++ math_app/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html')
+    result = None
+    if request.method == 'POST':
+        # ከ HTML የመጣውን ቁጥር መቀበል
+        input_num = request.POST.get('number')
+        if input_num:
+            try:
+                # በ 32 ማባዛት
+                result = float(input_num) * 32
+                # ውጤቱ የአስርዮሽ ቁጥር ከሆነ እንዲያጥር (.00) ማድረግ
+                if result.is_integer():
+                    result = int(result)
+            except ValueError:
+                result = "ስህተት፡ እባክህ ትክክለኛ ቁጥር አስገባ"
+                
+    return render(request, 'index.html', {'result': result})
